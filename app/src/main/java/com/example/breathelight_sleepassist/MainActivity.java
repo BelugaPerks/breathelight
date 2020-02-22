69bbf019d96b268aeb2a38fd6906adf4e65ba2b8
Rory Perks <rory.perks1@gmail.com>
2020-02-22 17:06:52 +0000

    added spinner for duration select

--- app/src/main/java/com/example/breathelight_sleepassist/MainActivity.java
+++ app/src/main/java/com/example/breathelight_sleepassist/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.breathelight_sleepassist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.timeSelector);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.time_array, R.layout.spinner_item);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
+
+
+
+
 }
