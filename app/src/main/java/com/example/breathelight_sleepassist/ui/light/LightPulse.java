e707b7e199feb66d34335ebd0d602f5f9a795073
Rory Perks <rory.perks1@gmail.com>
2020-03-11 16:57:48 +0000

    fix light pulse animations stopping after 5 mins, add annotations

--- app/src/main/java/com/example/breathelight_sleepassist/ui/light/LightPulse.java
+++ app/src/main/java/com/example/breathelight_sleepassist/ui/light/LightPulse.java
@@ -1,4 +1,4 @@
-package com.example.breathelight_sleepassist.ui.home;
+package com.example.breathelight_sleepassist.ui.light;
 
 import android.animation.Animator;
 import android.animation.AnimatorInflater;
@@ -29,6 +29,8 @@ public class LightPulse extends Activity {
     public LightPulse(){
 
     }
+
+    //Hides the status bar to allow true black background
     public void hideStatusBar() {
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
             getWindow().getDecorView()
@@ -48,10 +50,13 @@ public class LightPulse extends Activity {
 
         setContentView(R.layout.light_pulse);
 
+        //prevent the screen from automatically locking before the light pulse duration is over
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
 
         hideStatusBar();
 
+        //Enable stop button
         Button stopButton = findViewById(R.id.stop_button);
         stopButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,81 +65,111 @@ public class LightPulse extends Activity {
             }
         });
 
+        //Get data passed to LightPulse activity from home UI
         Intent intent = getIntent();
+        //Set background to selected colour
         final ImageView lightPulseImage = findViewById(R.id.image_pulsing_light);
         lightPulseImage.setBackgroundColor(intent.getIntExtra("colour", 0));
-        final Animation fadeLoop = AnimationUtils.loadAnimation( this.getApplicationContext(), R.anim.fade_loop);
-        //final ObjectAnimator fade = (ObjectAnimator) AnimatorInflater.loadAnimator(this.getApplicationContext(), R.animator.fade);
-        Integer duration = Integer.valueOf(intent.getStringExtra("duration").substring(0,2))*60000;
 
 
-        Integer startBPMDuration = 60000/intent.getIntExtra("startBPM", 0);
-        Integer goalBPMDuration = 60000/intent.getIntExtra("goalBPM", 0);
+        //Get total duration, start breath duration, and goal breath duration in milliseconds.
+        int duration = Integer.valueOf(intent.getStringExtra("duration").substring(0,2))*60000;
+        Integer startBreathDuration = 60000/intent.getIntExtra("startBPM", 0);
+        Integer goalBreathDuration = 60000/intent.getIntExtra("goalBPM", 0);
 
-        Integer BPMDelta = goalBPMDuration - startBPMDuration;
-        Integer averageBPM = (startBPMDuration + goalBPMDuration)/2;
+        //Calculate difference in milliseconds between start and goal duration.
+        Integer BreathDelta = goalBreathDuration - startBreathDuration;
+        //Calculate the average breath length when moving from start to goal. This is just the midpoint.
+        int averageBreathDuration = (startBreathDuration + goalBreathDuration)/2;
 
-        Log.i("debug", "onCreate: Start BPM duration: " + startBPMDuration + " Goal BPM duration: " + goalBPMDuration + " DPM Delta: " + BPMDelta + " Average BPM: " + averageBPM + " total duration: " + duration);
+        Log.i("debug", "onCreate: Start Breath duration: " + startBreathDuration + " Goal Breath duration: " + goalBreathDuration + " Breath Delta: " + BreathDelta + " Average Breath DUration: " + averageBreathDuration + " total duration: " + duration);
 
-        Integer breathsInFiveMinutesAverage = 300000/averageBPM;
+        //Transition period is 5 mins, need to calculate the number of breaths in 5 mins if all breaths were at average
+        Integer breathsInFiveMinutesAverage = 300000/averageBreathDuration;
 
         Log.i("debug", "onCreate: Average breaths in 5 minutes: " + breathsInFiveMinutesAverage);
 
-        Integer breathDurationShift = BPMDelta/breathsInFiveMinutesAverage;
+        //Calculate how much to decrease breath duration by each time, in order to be consistent and move to goal breath duration in 5 mins
+        Integer breathDurationShift = BreathDelta/breathsInFiveMinutesAverage;
 
         Log.i("debug", "onCreate: Breath duration shift: " + breathDurationShift);
 
+        //Instantiate animatorSet for playing sequential animators
         AnimatorSet pulseSet = new AnimatorSet();
+        //Instantiate animator list to store pulsing light animators
         List<Animator> animations = new ArrayList<>();
 
+        //Integer for tracking how long the transition set of animators takes in milliseconds
         Integer totalTimeForFirstAnimations = 0;
 
+        //Mutable breath duration integer for transition from start to goal BPM in for loop
+        int breathDuration = startBreathDuration;
+
+        //For loop to create the initial set of animators for the transition between start and goal BPM, over 5 minutes
         for(int i = 0; i<breathsInFiveMinutesAverage; i++){
-            Log.i("debug", "onCreate: " + startBPMDuration);
+            Log.i("debug", "onCreate: " + breathDuration);
+            //Create the animator to fade in and out once and add it to the list
             final ObjectAnimator fadeAnimator = ObjectAnimator
                     .ofFloat(lightPulseImage, View.ALPHA, 0f, 1f)
-                    .setDuration(startBPMDuration);
+                    .setDuration(breathDuration);
             fadeAnimator.setRepeatCount(1);
             fadeAnimator.setRepeatMode(ValueAnimator.REVERSE);
             animations.add(fadeAnimator);
-            totalTimeForFirstAnimations = totalTimeForFirstAnimations + startBPMDuration;
-            startBPMDuration = startBPMDuration + breathDurationShift;
+            //update the duration for the next animation by adding on the previously calculated shift
+            breathDuration = breathDuration + breathDurationShift;
+            //update total time for transition animations
+            totalTimeForFirstAnimations = totalTimeForFirstAnimations + breathDuration;
         }
         Log.i("debug", "onCreate: Total time for first set of animations: " + totalTimeForFirstAnimations);
-        int remainingDuration = duration-300000;
-        int numberOfRepeatsRemaining = remainingDuration/goalBPMDuration;
+        //Calculate remaining time after transition is done, for debugging
+        int remainingDuration = duration-totalTimeForFirstAnimations;
+        //Calculate how many breaths can be done in the remaining time at the goal breath duration
+        int numberOfRepeatsRemaining = remainingDuration/goalBreathDuration;
+
         Log.i("debug", "onCreate: remaining duration: " + remainingDuration + " Number of repeats: " + numberOfRepeatsRemaining);
-        Log.i("debug", "onCreate: GoalBPM: " + goalBPMDuration);
+        Log.i("debug", "onCreate: GoalBPM: " + goalBreathDuration);
+        //Ensure the number of remaining repeats is odd, to prevent ending with light permanently on
         if(numberOfRepeatsRemaining%2 == 0){
             numberOfRepeatsRemaining=numberOfRepeatsRemaining+1;
         }
         Log.i("debug", "onCreate: remaining duration: " + remainingDuration + " Number of repeats: " + numberOfRepeatsRemaining);
 
+        //Create animator which repeats for the remaining number of breaths, with 10 extra added to allow the screen to automatically lock before the process is finished
         final ObjectAnimator fadeAnimator = ObjectAnimator
                 .ofFloat(lightPulseImage, View.ALPHA, 0f, 1f)
-                .setDuration(goalBPMDuration);
-        fadeAnimator.setRepeatCount(numberOfRepeatsRemaining+6);
+                .setDuration(goalBreathDuration);
+        fadeAnimator.setRepeatCount(numberOfRepeatsRemaining+10);
         fadeAnimator.setRepeatMode(ValueAnimator.REVERSE);
         animations.add(fadeAnimator);
 
         Log.i("debug", "onCreate: animations list size: " + animations.size());
 
-        int totalAnimationTime = totalTimeForFirstAnimations + (numberOfRepeatsRemaining*goalBPMDuration);
+        //Calculate the total time of animations in milliseconds, for debugging
+        int totalAnimationTime = totalTimeForFirstAnimations + (numberOfRepeatsRemaining*goalBreathDuration);
 
         Log.i("debug", "onCreate: animations total time in milliseconds: " + totalAnimationTime);
 
+        //After the chosen duration is over, allow the screen to lock.
         final Handler screenOffHandler = new Handler();
         screenOffHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Log.i("debug", "times up");
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-                lightPulseImage.setVisibility(View.GONE);
+
+                //End the activity once the screen has  been given time to lock
+                final Handler screenOffHandler = new Handler();
+                screenOffHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.i("debug", "Exiting light pulse activity");
+                        finish();
+                    }
+                }, 30000);
             }
         }, duration);
 
-
-
+        //Add the animator list to the animatorSet, and play them sequentially
         pulseSet.playSequentially(animations);
         pulseSet.start();
 
