d001677e78613a6b652e86c8d9477925b4aea413
BelugaPerks <61022226+BelugaPerks@users.noreply.github.com>
2021-12-16 16:31:16 +0000

    Remove rate view, implement in-app rating flow once user has used the app to do a full length light pulse activity at least 3 times.

--- breatheLight/src/main/java/com/belugaperks/breathelight_sleepassist/MainActivity.java
+++ breatheLight/src/main/java/com/belugaperks/breathelight_sleepassist/MainActivity.java
@@ -1,9 +1,12 @@
 package com.belugaperks.breathelight_sleepassist;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.belugaperks.breathelight_sleepassist.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -15,6 +18,10 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.play.core.review.ReviewInfo;
+import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.review.ReviewManagerFactory;
+import com.google.android.play.core.tasks.Task;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -25,11 +32,16 @@ import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ReviewManager reviewManager;
+
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        reviewManager = ReviewManagerFactory.create(this);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -39,13 +51,36 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_about, R.id.nav_share, R.id.nav_rate)
+                R.id.nav_home, R.id.nav_about, R.id.nav_share)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+
+        // Get persistent count var, if it is 3 then can ask the user to rate the app.
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.preferences_file_key), Context.MODE_PRIVATE );
+        int count = prefs.getInt("activityCount", 0);
+        System.out.println("Count: " + count);
+        if(count > 3){
+            Task<ReviewInfo> request = reviewManager.requestReviewFlow();
+            request.addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    // We can get the ReviewInfo object
+                    ReviewInfo reviewInfo = task.getResult();
+                    Task<Void> flow = reviewManager.launchReviewFlow(this, reviewInfo);
+                    flow.addOnCompleteListener(task2 -> {
+                        // The flow has finished. The API does not indicate whether the user
+                        // reviewed or not, or even whether the review dialog was shown. Thus, no
+                        // matter the result, we continue our app flow.
+                    });
+                } else {
+                    // There was some problem, continue regardless of the result.
+                }
+            });
+
+        }
     }
 
     @Override
--- breatheLight/src/main/java/com/belugaperks/breathelight_sleepassist/ui/light/LightPulse.java
+++ breatheLight/src/main/java/com/belugaperks/breathelight_sleepassist/ui/light/LightPulse.java
@@ -5,7 +5,9 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -18,26 +20,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.belugaperks.breathelight_sleepassist.R;
+import com.google.android.play.core.review.ReviewInfo;
+import com.google.android.play.core.review.ReviewManager;
+import com.google.android.play.core.review.ReviewManagerFactory;
+import com.google.android.play.core.tasks.Task;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class LightPulse extends AppCompatActivity {
+
+
     public LightPulse(){
 
     }
 
     // Hides the status bar to allow true black background
     public void hideStatusBar() {
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-            getWindow().getDecorView()
-                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    );
-        }
+        getWindow().getDecorView()
+                .setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                );
     }
 
 
@@ -45,6 +52,9 @@ public class LightPulse extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState){
+
+
+
         super.onCreate(savedInstanceState);
 
         setTheme(R.style.AppTheme);
@@ -83,17 +93,28 @@ public class LightPulse extends AppCompatActivity {
             public void run() {
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-                //End the activity once the screen has  been given time to lock
+                //End the activity once the screen has been given time to lock
                 final Handler screenOffHandler = new Handler();
                 screenOffHandler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         Log.i("debug", "Exiting light pulse activity");
+
+                        // Create/update persistent count var
+                        SharedPreferences prefs = getSharedPreferences(getString(R.string.preferences_file_key), Context.MODE_PRIVATE );
+                        int count = prefs.getInt("activityCount", 0);
+                        count++;
+                        SharedPreferences.Editor editor = prefs.edit();
+                        editor.putInt("activityCount", count );
+                        editor.apply();
                         finish();
                     }
                 }, 30000);
             }
         }, duration);
+
+
+
 
         // Get list of animations to play for pulsing breathing lights.
         List<Animator> animations = lightPulseViewModel.generateAnimatorList(intent, lightPulseImage);
